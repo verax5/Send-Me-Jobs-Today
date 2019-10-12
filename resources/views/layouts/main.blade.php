8cd06e7906e748e8465c8ada48caa5c0ae21f5a6
Sami <phpdevsami@gmail.com>
2019-10-12 16:48:56 +0100

    google analytics

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -7,7 +7,16 @@
         <link rel="stylesheet" type="text/css" href="/css/style.css">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        
+
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-110222642-2"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'UA-110222642-2');
+        </script>
     </head>
 
     <body>
