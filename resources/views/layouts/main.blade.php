0a4fde3017d52dca5f0c8920c7572bffc8d50171
Sami <phpdevsami@gmail.com>
2019-10-19 00:25:45 +0100

    ads

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -18,6 +18,13 @@
           gtag('config', 'UA-110222642-3');
         </script>
 
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        <script>
+             (adsbygoogle = window.adsbygoogle || []).push({
+                  google_ad_client: "ca-pub-4947569401603583",
+                  enable_page_level_ads: true
+             });
+        </script>
     </head>
 
     <body>
