8fbb1e25ef2108f982bd34366679836131c07c00
Sami <phpdevsami@gmail.com>
2019-11-17 13:30:29 +0000

    removing desc

--- resources/views/job_details.blade.php
+++ resources/views/job_details.blade.php
@@ -6,13 +6,15 @@
 
     <div class="row">
     	<div class="col-md-12">
-    		<h2>{{request()->get('title')}}</h2> Location: {{request()->location}} <br> <br
-    		<p>As a {{request()->title}} you will be working on various projects which includes working on greenfield projects aswell as legacy. You're also expected to have good understanding of MySQL aswell as HTML/CSS and Javascript along with good communication skills.</p>
+    		<h2 class="text-center">{{request()->get('title')}}</h2> {{--Location: {{request()->location}} --}}
+    			
+    			{{-- <p>As a {{request()->title}} you will be working on various projects which includes working on greenfield projects aswell as legacy. You're also expected to have good understanding of MySQL aswell as HTML/CSS and Javascript along with good communication skills.</p> 
+    		--}}
 
-    		<p>{!! $jobDetails->snippet !!}</p>
-    		
+    		{{-- <p>{!! $jobDetails->snippet !!}</p> --}}
+    		<br>
     		<div class="text-center">
-       			<a onclick="countClick();" target="_blank" class="job_titles" href="{{ $jobDetails->url }}" onmousedown="{{ $jobDetails->onmousedown }}"><button style="padding-left:50px; padding-right:50px; background-color:#dd5800; font-size:20px; text-decoration: underline;" class="btn btn-info">Apply</button></a>
+       			<a onclick="countClick();" target="_blank" class="job_titles" href="{{ $jobDetails->url }}" onmousedown="{{ $jobDetails->onmousedown }}"><button style="padding-left:50px; padding-right:50px; background-color:#dd5800; font-size:20px; text-decoration: underline;" class="btn btn-info">Read Description..</button></a>
        		</div>
     	</div>
 
