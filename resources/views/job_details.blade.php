9615e936bc3ac9a981ce0d13cf6dac8205f38ad7
Sami <phpdevsami@gmail.com>
2019-11-20 20:43:09 +0000

    snippet

--- resources/views/job_details.blade.php
+++ resources/views/job_details.blade.php
@@ -8,10 +8,10 @@
     	<div class="col-md-12">
     		<h2 class="text-center">{{request()->get('title')}}</h2> {{--Location: {{request()->location}} --}}
     			
-    			{{-- <p>As a {{request()->title}} you will be working on various projects which includes working on greenfield projects aswell as legacy. You're also expected to have good understanding of MySQL aswell as HTML/CSS and Javascript along with good communication skills.</p> 
-    		--}}
+    			 {{-- <p>As a {{request()->title}} you will be working on various projects which includes working on greenfield projects aswell as legacy. You're also expected to have good understanding of MySQL aswell as HTML/CSS and Javascript along with good communication skills.</p> --}}
+    		
 
-    		{{-- <p>{!! $jobDetails->snippet !!}</p> --}}
+    		 <p>{!! $jobDetails->snippet !!}</p>
     		<br>
     		<div class="text-center">
        			<a onclick="countClick();" target="_blank" class="job_titles" href="{{ $jobDetails->url }}" onmousedown="{{ $jobDetails->onmousedown }}"><button style="padding-left:50px; padding-right:50px; background-color:#dd5800; font-size:20px; text-decoration: underline;" class="btn btn-info">Read Description..</button></a>
