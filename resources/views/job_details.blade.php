c465c20db99eba81c3d014b6052b74512b3200fa
Sami <phpdevsami@gmail.com>
2020-01-04 17:00:11 +0000

    improvements to page

--- resources/views/job_details.blade.php
+++ resources/views/job_details.blade.php
@@ -6,6 +6,7 @@
 	<div class="row text-center">
 		<div class="col-md-12 mt-2 mb-2">
 			<h2><b>{{ strtoupper(request()->get('title')) }}</b></h2>
+			<b><a style="text-decoration: underline; color:#3800ff;" href="/search?keyword={{request()->get('keyword')}}&location={{request()->get('location')}}">More jobs? <i>SEE HERE</i></a></b>
 		</div>
 	</div>
 
@@ -17,7 +18,8 @@
 		   			<a onclick="countClick(); fbq('track', 'Purchase');" target="_blank" class="job_titles" href="{{ $job->url }}" onmousedown="{{ $job->onmousedown }}"><button style="padding-left:50px; padding-right:50px; background-color:#dd5800; font-size:20px; text-decoration: underline;" class="btn btn-info">Read Description..</button></a>
 		   		</div>
 			</div>	
-		</div> <hr>
+		</div>
+		 <hr>
 	@endforeach
 
     <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
